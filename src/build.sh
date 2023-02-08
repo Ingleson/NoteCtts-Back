afa8dcdad414613ca290bfa0bfa592f828f85190
Ingleson <jovemtksstranger@gmail.com>
2023-02-08 00:51:47 -0400

    build constructed

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,4 @@
 set -o errexit
-yarn typeorm migration:generate -d src/data-source.ts src/migrations/initialMigration
+yarn typeorm migration:create src/migrations/initialMigration     
+yarn typeorm migration:run -d src/data-source.ts
 yarn dev
