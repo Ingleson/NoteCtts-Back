4eafe9b9dd6918d5fb059fbe1a126fce681d1197
Ingleson <jovemtksstranger@gmail.com>
2023-02-08 14:23:31 -0400

    .env modify

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,7 +4,7 @@ require('dotenv').config()
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
-  port: 5432,
+  port: Number(process.env.POSTGRES_PORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
